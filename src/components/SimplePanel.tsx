8362d729c0189392665812b148a01146fa21fe21
João Eirinha <joaoeirinh@gmail.com>
2023-03-21 16:47:19 +0000

    add replace variables

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -5,7 +5,7 @@ import { Menu } from '@grafana/ui';
 
 interface Props extends PanelProps<Options> {}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height, replaceVariables }) => {
   return (
     <Menu>
       {options.menu.map((menuItem, index) => {
@@ -13,7 +13,11 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
           <Menu.Group key={index} label={menuItem.name}>
             {menuItem.subMenu.map((subMenuItem, subIndex) => {
               return (
-                <Menu.Item key={subIndex} label={subMenuItem.name} onClick={() => (location.href = subMenuItem.link)} />
+                <Menu.Item
+                  key={subIndex}
+                  label={replaceVariables(subMenuItem.name)}
+                  onClick={() => (location.href = replaceVariables(subMenuItem.link))}
+                />
               );
             })}
           </Menu.Group>
